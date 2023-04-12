c55cd47fb997e5668f3e10b36f54e7b78e8f3627
zulujumper <zulujumper@gmail.com>
2023-04-12 14:25:20 -0700

    Week 4 homework upload

---
+++ Week4/index.js
@@ -0,0 +1,6 @@
+document.getElementById("button").addEventListener(
+    "click",
+    () => {
+        let email = document.getElementById('headline');
+        email.innerHTML += "drewp.hansen@centurylink.net";
+    )}
